8521f10ef1a9495c4f3d68325ece6b89bea79bce
mikairis <thong.michael@gmail.com>
2017-08-10 00:02:15 +0800

    task 5 for workshop 4

--- Workshop4/Program.cs
+++ Workshop4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Workshop4
 {
@@ -6,7 +7,7 @@ namespace Workshop4
     {
         static void Main(string[] args)
         {
-            string[,] name = { { "geoff", "dick", "harry"},
+            /*string[,] name = { { "geoff", "dick", "harry"},
                                { "liam", "sam", "jack" }
             };
 
@@ -24,8 +25,45 @@ namespace Workshop4
                     Console.Write(name[i,j] + " ");
                 }
             }
+            Console.ReadKey();*/
+
+            Console.WriteLine("*********This is for Workshop 4 Task 4********");
+
+            string nusISS = "institute of systems science";
+
+            TextInfo textInfo = new CultureInfo("en-us", false).TextInfo;
+            nusISS = textInfo.ToTitleCase(nusISS);
+
+            Console.WriteLine(nusISS);
             Console.ReadKey();
+
+            Console.WriteLine();
+
+            Console.WriteLine("**********************This is for Workshop 4 Task 5***********************");
+            Console.WriteLine("*********This is a programme for listing student's name and grades********");
+            Console.WriteLine();
+            Console.Write("Please enter the number of students: ");
+            int studNum = Convert.ToInt32(Console.ReadLine());
+            string[] studentName = new string[studNum];
+
+            Console.WriteLine();
             
+            int[] studentMarks = new int[studNum];
+
+            for (int i = 0; i < studNum; i++)
+            {
+                Console.Write("Enter student's name: ");
+                studentName[i] = Console.ReadLine();
+                
+                Console.Write("Enter student's marks: ");
+                studentMarks[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            for (int i = 0; i < studNum; i++)
+            {
+                Console.Write(studentName[i] + "\t" + studentMarks[i] + "\n");
+            }
+            Console.ReadKey();
         }
     }
 }
