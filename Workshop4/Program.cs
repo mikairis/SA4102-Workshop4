ba5c3febdbe4e9ead8a88af20919915d7ee43f6d
mikairis <thong.michael@gmail.com>
2017-08-09 12:31:37 +0800

    test array codes

--- Workshop4/Program.cs
+++ Workshop4/Program.cs
@@ -6,9 +6,26 @@ namespace Workshop4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world!");
+            string[,] name = { { "geoff", "dick", "harry"},
+                               { "liam", "sam", "jack" }
+            };
 
+            int x = 2;
+            int y = 3;
+
+            //int[,] number = {   { 1, 2, 3 },
+            //                    { 4, 5, 6 }
+            //};
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write(name[i,j] + " ");
+                }
+            }
             Console.ReadKey();
+            
         }
     }
 }
